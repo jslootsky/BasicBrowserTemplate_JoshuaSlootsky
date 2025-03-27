5b82a122af6497b4784e0574444eb9ff3bdef657
jslootsky <joshuaslootsky@gmail.com>
2025-03-27 12:18:12 -0400

    created the browser

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -6,6 +6,9 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,11 +24,37 @@ class MainActivity : AppCompatActivity() {
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
 
+
         // Allow your browser to intercept hyperlink clicks
         webView.webViewClient = object: WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
             }
+        }
+
+        goButton.setOnClickListener{
+            var original = urlEditText.text.toString()
+            //check if URL starts with https://
+            if(!original.startsWith("https://")){
+                original = "https://" + original
+            }
+
+            webView.loadUrl(original)
+//            val url = URL(original.toString())
+//
+//            val response = url.openStream().bufferedReader().readLine().run{
+//                val strBuilder = StringBuilder()
+//
+//                while(readLine().let{
+//                        strBuilder.append("$it\n")
+//                        it != null
+//                    })
+//                    strBuilder.toString()
+//            }
+//
+//            withContext(Dispatchers.Main){
+//                webView.text = response
+//            }
         }
 
     }
